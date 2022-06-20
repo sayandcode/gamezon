eb1f60569b6890a8f9ce8b61bdbe953ff429142a
sayandcode <sayandcode@gmail.com>
2022-06-20 17:40:11 +0530

    Add prices scraper
    Scrapes top website to buy for the price. Also updates the release date
    to null, if you couldn't find the price. I.e its not yet released

--- scraper/getPicsNPrices.js
+++ scraper/getPicsNPrices.js
@@ -6,7 +6,9 @@ const {
   getYoutubeURL,
   getGameScreenshots,
   getBoxArtImage,
+  getPriceAccToConsole,
 } = require('./helperFns/picsNPricesHelpers');
+const sleep = require('./helperFns/sleep');
 
 /* THINGS TO SCRAPE */
 //  1 Box Art Image
@@ -22,27 +24,44 @@ puppeteer.use(AdblockerPlugin());
 (async () => {
   const db = JSON.parse(await fs.readFile(DB_URL));
   const gameNames = Object.keys(db);
-  for (let i = 0; i < /* gameNames.length */ 1; i += 1) {
+  for (let i = 2; i < /* gameNames.length */ 3; i += 1) {
     const gameName = gameNames[i];
 
     /* Source the data */
     //  1 Box Art Image
     const boxArtImage = await getBoxArtImage(gameName);
     //  1 Game trailer youtube URL
-    /* const youtubeURL = await getYoutubeURL(`${gameName} video game trailer`); */
+    const youtubeURL = await getYoutubeURL(`${gameName} video game trailer`);
+    db[gameName].trailerURL = youtubeURL;
+
     //  4 Game screenshot images
-    /* const gameScreenshots = await getGameScreenshots(gameName); */
+    const gameScreenshots = await getGameScreenshots(gameName);
 
     //  price foreach console
+    const { variants } = db[gameName];
+    for (let j = 0; j < variants.length; j += 1) {
+      const thisVariant = variants[j];
+      const price = await getPriceAccToConsole(
+        gameName,
+        thisVariant.consoleName
+      );
+      thisVariant.price = price;
+      if (!price) thisVariant['Release date'] = null;
+      await sleep(500);
+    }
 
     /* Now write everything to disk */
-    const dir = `./pics/${gameName}`;
-    await fs.mkdir(dir, { recursive: true });
-    /* gameScreenshots.forEach(async (screenshot, index) =>
-      fs.writeFile(`${dir}/${index + 1}.png`, screenshot)
-    ); */
-    await fs.writeFile(`${dir}/boxArt.png`, boxArtImage);
+    const picsDir = `./pics/${gameName}`;
+    await fs.mkdir(picsDir, { recursive: true });
+    gameScreenshots.forEach(async (screenshot, index) =>
+      fs.writeFile(`${picsDir}/${index + 1}.png`, screenshot)
+    );
+    await fs.writeFile(`${picsDir}/boxArt.png`, boxArtImage);
+    console.log(`Finished${i + 1} of ${gameNames.length} `);
   }
-})();
 
-// id='contents'
+  // write to the disk last cause anyway its stored in memory
+  const dataDir = './gameDataJSON';
+  await fs.mkdir(dataDir, { recursive: true });
+  await fs.writeFile(`${dataDir}/gameDataWithPrices.json`, JSON.stringify(db));
+})();
--- scraper/helperFns/picsNPricesHelpers.js
+++ scraper/helperFns/picsNPricesHelpers.js
@@ -18,6 +18,7 @@ async function getYoutubeURL(queryString) {
     )}`
   );
   await handleCaptchasIn(page);
+  await sleep(100);
   const URL = await page.$eval('#contents a', (a) => a.href);
   await browser.close();
   return URL;
@@ -59,6 +60,45 @@ async function getBoxArtImage(gameName) {
   return boxArtImage;
 }
 
+async function getPriceAccToConsole(gameName, consoleName) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+  await handleCaptchasIn(page);
+
+  const queryString = `${gameName} ${consoleName} price usa`;
+  await page.type('input[aria-label="Search"]', queryString);
+  await page.evaluate(() => {
+    const imFeelingLuckyBtn = document.querySelector(
+      `input[aria-label="I'm Feeling Lucky"]`
+    );
+    imFeelingLuckyBtn.click();
+  });
+  await page.waitForNavigation();
+  if (page.url().match(/en-in/)) {
+    const newURL = page.url().replace(/en-in/g, 'en-us');
+    await page.goto(newURL);
+  }
+
+  const priceElement = (
+    await page.$x(
+      "//*[(self::p or self::div or self::span)][contains(text(),'$')]"
+    )
+  )?.[0];
+
+  const price = priceElement
+    ? await priceElement.evaluate((el) => el.innerText.match(/(\$[\d,.]+)/)[0])
+    : null;
+
+  await browser.close();
+
+  return price;
+}
+
 exports.getYoutubeURL = getYoutubeURL;
 exports.getGameScreenshots = getGameScreenshots;
 exports.getBoxArtImage = getBoxArtImage;
+exports.getPriceAccToConsole = getPriceAccToConsole;
