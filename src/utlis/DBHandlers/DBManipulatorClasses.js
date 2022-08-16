5f5f76bdd4ca511ef53a104f8c8720c93d039235
sayandcode <sayandcode@gmail.com>
2022-08-16 16:05:59 +0530

    Refactor DBManipulatorClasses to use factory function
      * It was too bloated. The main logic was extracted, which reduced code
    reuse. Now each DB Manipulator is an instance of the root class

--- src/utlis/DBHandlers/DBManipulatorClasses.js
+++ src/utlis/DBHandlers/DBManipulatorClasses.js
@@ -4,8 +4,18 @@ import { firestoreDB } from '../firebase-config';
 const USERS_DB_COLLECTION_NAME = 'users';
 const GAME_DB_COLLECTION_NAME = 'games';
 
-class DatabaseManipulator {
-  static async getDocument({ docName }) {
+class DBManipulator {
+  #collectionName;
+
+  constructor(collectionName) {
+    this.#collectionName = collectionName;
+  }
+
+  get collectionName() {
+    return this.#collectionName;
+  }
+
+  async getDocument({ docName }) {
     const docRef = doc(firestoreDB, this.collectionName, docName);
     const fetchedDoc = await getDoc(docRef);
     if (fetchedDoc.exists())
@@ -13,53 +23,34 @@ class DatabaseManipulator {
     return undefined;
   }
 
-  static async deleteDocument({ docName }) {
+  async deleteDocument({ docName }) {
     const docRef = doc(firestoreDB, this.collectionName, docName);
     return deleteDoc(docRef);
   }
 
-  static async setDocument({ docName, data }) {
+  async setDocument({ docName, data }) {
     const docRef = doc(firestoreDB, this.collectionName, docName);
     return setDoc(docRef, data);
   }
 }
 
-export class UsersDatabase extends DatabaseManipulator {
-  static #collectionName = USERS_DB_COLLECTION_NAME; // private so that it cant be changed
-
-  static get collectionName() {
-    return this.#collectionName;
-  }
-
-  static async delete({ userID }) {
-    return this.deleteDocument({ docName: userID });
-  }
-
-  static async set({ userID, data }) {
-    return this.setDocument({ docName: userID, data });
-  }
-
-  static async get({ userID }) {
-    return this.getDocument({ docName: userID });
-  }
+function createDBManipulator(collectionName, docNameAlias) {
+  const DBManipInstance = new DBManipulator(collectionName);
+  const extendedMethods = {
+    get({ [docNameAlias]: docName }) {
+      return this.getDocument({ docName });
+    },
+    set({ [docNameAlias]: docName, data }) {
+      return this.setDocument({ docName, data });
+    },
+    delete({ [docNameAlias]: docName }) {
+      return this.deleteDocument({ docName });
+    },
+  };
+  return Object.assign(DBManipInstance, extendedMethods);
 }
 
-export class GameDatabase extends DatabaseManipulator {
-  static #collectionName = GAME_DB_COLLECTION_NAME; // private so that it cant be changed
+const UsersDatabase = createDBManipulator(GAME_DB_COLLECTION_NAME, 'userID');
+const GameDatabase = createDBManipulator(USERS_DB_COLLECTION_NAME, 'title');
 
-  static get collectionName() {
-    return this.#collectionName;
-  }
-
-  static async delete({ title }) {
-    return this.deleteDocument({ docName: title });
-  }
-
-  static async set({ title, data }) {
-    return this.setDocument({ docName: title, data });
-  }
-
-  static async get({ title }) {
-    return this.getDocument({ docName: title });
-  }
-}
+export { UsersDatabase, GameDatabase };
